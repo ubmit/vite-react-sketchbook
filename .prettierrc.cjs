af997e8661f3c72d1670681a4f3e5144ac80ffd1
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-03-07 01:18:10 +0000

    chore: install prettier-plugin-tailwindcss

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,6 +2,7 @@
 const config = {
   semi: false,
   singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
 }
 
 module.exports = config
--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,29 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="max-w-screen-lg text-white min-h-svh flex flex-col items-center justify-center gap-8 bg-gray-800">
+    <div className="max-w-screen-lg flex min-h-svh flex-col items-center justify-center gap-8 bg-gray-800 text-white">
       <div className="flex">
         <a href="https://vitejs.dev" target="_blank">
           <img
             src={viteLogo}
-            className="h-36 will-change-[filter] transition-[filter] duration-300 p-6 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            className="h-36 p-6 transition-[filter] duration-300 will-change-[filter] hover:drop-shadow-[0_0_2em_#646cffaa]"
             alt="Vite logo"
           />
         </a>
         <a href="https://react.dev" target="_blank">
           <img
             src={reactLogo}
-            className="h-36 will-change-[filter] transition-[filter] duration-300 p-6 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-spin-slow"
+            className="animate-spin-slow h-36 p-6 transition-[filter] duration-300 will-change-[filter] hover:drop-shadow-[0_0_2em_#61dafbaa]"
             alt="React logo"
           />
         </a>
       </div>
-      <h1 className="text-5xl text-white font-bold leading-tight">
+      <h1 className="text-5xl font-bold leading-tight text-white">
         Vite + React
       </h1>
       <div className="flex flex-col items-center gap-4">
         <button
-          className="border text-white bg-gray-950 transition-colors duration-200 px-5 py-2 rounded-lg border-solid border-transparent hover:border-blue-400"
+          className="rounded-lg border border-solid border-transparent bg-gray-950 py-2 px-5 text-white transition-colors duration-200 hover:border-blue-400"
           onClick={() => setCount((count) => count + 1)}
         >
           count is {count}
